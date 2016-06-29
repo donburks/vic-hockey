f8b827a6217849dd900e89f9d2cdfaef970adf1c
Don Burks <don@donburks.com>
2016-06-29 14:00:36 -0700

    Refactored JS into more OOPsy style

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,68 +1,73 @@
 $(function() {
   var table = $("#canucks").find('tbody');
 
-  function displayPlayer(player) {
-    var tr = $("<tr>").appendTo(table);
-
-    $("<td>").text(player.number).appendTo(tr); 
-    $("<td>").text(player.name).appendTo(tr); 
-    $("<td>").text(player.position).appendTo(tr); 
-    $("<td>").text(player.height).appendTo(tr); 
-    $("<td>").text(player.weight).appendTo(tr); 
-    $("<button>").text("Delete Player").data('id', player.id).appendTo(tr).addClass('deleteBtn');
-
+  function playerRow(data, tr) {
+    $("<td>").text(data).appendTo(tr);
   }
 
-  $("#loadPlayers").on('click', function() {
-    $.getJSON('/players', function(players) {
-      table.empty();
-      players.forEach(displayPlayer);
-      $("#playerForm").hide();
-      $("#results").show();
-    });
-  });
+  var handlers = {
+    displayPlayer: function(player) {
+      var tr = $("<tr>").appendTo(table);
 
-  $("#canucks").on('click', 'button.deleteBtn', function() {
-    var btn = $(this);
-    var id = btn.data('id');
-    $.getJSON('/players/'+id+'/delete', function(results) {
-      if (results.result) {
-        alert("Deleted player.");
-        btn.parents('tr').remove();
-      } else {
-        alert("WTF");
-      }
-    });
-  });
+      ["number", "name", "position", "height", "weight"].map(function(field) {
+        playerRow(player[field], tr);
+      });
 
-  $("#addPlayer").on('click', function() {
-    $("#results").hide();
-    $("#playerForm").show(); 
-  });
-
-  $("#newPlayer").on('submit', function(evt) {
-    evt.preventDefault();
-    var jersey = $("#jersey").val();
-    var name = $("#name").val();
-    var position = $("#position").val();
-    if (jersey !== '' && name !== '' && position !== '') {
-      $.post('/players/create', {number: jersey, name: name, position: position}, function(results) {
+      $("<button>").text("Delete Player").data('id', player.id).appendTo(tr).addClass('deleteBtn');
+    },
+    loadPlayers: function() {
+      $.getJSON('/players', function(players) {
+        table.empty();
+        players.forEach(handlers.displayPlayer);
+        $("#playerForm").hide();
+        $("#results").show();
+      });
+    },
+    deleteBtn: function() {
+      var btn = $(this);
+      var id = btn.data('id');
+      $.getJSON('/players/'+id+'/delete', function(results) {
         if (results.result) {
-          alert("Added player with ID: " + results.id);
+          alert("Deleted player.");
+          btn.parents('tr').remove();
         } else {
           alert("WTF");
         }
-      }, 'json');
-    } 
-  });
-
-  $("#search").on('keyup', function(evt) {
-    if (evt.keyCode == 13) {
-      var query = $(this).val();
-      $.getJSON('/search/'+query, function(players) {
-        table.empty();
-        players.forEach(displayPlayer);
       });
+    },
+    addPlayer: function() {
+      $("#results").hide();
+      $("#playerForm").show(); 
+    },
+    newPlayer: function(evt) {
+      evt.preventDefault();
+      var jersey = $("#jersey").val();
+      var name = $("#name").val();
+      var position = $("#position").val();
+      if (jersey !== '' && name !== '' && position !== '') {
+        $.post('/players/create', {number: jersey, name: name, position: position}, function(results) {
+          if (results.result) {
+            alert("Added player with ID: " + results.id);
+          } else {
+            alert("WTF");
+          }
+        }, 'json');
+      } 
+    },
+    search: function(evt) {
+      if (evt.keyCode == 13) {
+        var query = $(this).val();
+        $.getJSON('/search/'+query, function(players) {
+          table.empty();
+          players.forEach(handlers.displayPlayer);
+        });
+      }
     }
-  });
+  };
+
+  $("#loadPlayers").on('click', handlers.loadPlayers);
+  $("#canucks").on('click', 'button.deleteBtn', handlers.deleteBtn);
+  $("#addPlayer").on('click', handlers.addPlayer);
+  $("#newPlayer").on('submit', handlers.newPlayer);
+  $("#search").on('keyup', handlers.search);
 });
