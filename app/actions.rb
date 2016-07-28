f52f9f412934b6d9b1e934edb69bd79ea4fcbe8b
Don Burks <don@donburks.com>
2016-07-28 12:08:07 -0600

    Lecture code

--- app/actions.rb
+++ app/actions.rb
@@ -7,6 +7,10 @@ get '/players' do
   Player.all.to_json	
 end
 
+get '/players/:id' do
+  Player.find(params[:id]).to_json
+end
+
 post '/players/create' do
   results = {result: false}
   player = Player.new number: params[:number], name: params[:name], position: params[:position]
@@ -21,6 +25,25 @@ end
 get '/search/:txt' do
   players = Player.where("name LIKE ?", "%#{params[:txt]}%") 
   players.to_json
+end
+
+post '/players/:id/update' do
+  player = Player.find(params[:id])
+  response = {success: false}
+  player.name = params[:name]
+  player.position = params[:position]
+  player.number = params[:number]
+  player.height = params[:height]
+  player.weight = params[:weight]
+  player.dob = params[:dob]
+  player.birthplace = params[:birthplace]
+  if player.save
+    response[:success] = true
+  else
+    response[:errors] = player.errors.full_messages
+  end
+
+  response.to_json
 end
 
 get '/players/:id/delete' do
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,2 +1,67 @@
 $(function() {
+  $("#loadPlayers").on('click', function() {
+    var table = $("table").find('tbody').empty();
+    $.getJSON("/players", function(players) {
+      players.forEach(function(player) {
+        var row = $("<tr>");
+        //Progressive Enhancement
+        var a = $("<a>").attr('href', '/players/'+player.id).text(player.name).addClass('playerLink');
+        $("<td>").append(a).appendTo(row);
+        $("<td>").text(player.number).appendTo(row);
+        $("<td>").text(player.position).appendTo(row);
+        $("<td>").text(player.height).appendTo(row);
+        $("<td>").text(player.weight).appendTo(row);
+        row.appendTo(table); 
+      });
+    });
+    $("#playerIndex").removeClass('hidden');
+    $("#playerShow").addClass('hidden');
+  });
+
+  $("table").on('click', ".playerLink", function(event) {
+    event.preventDefault();
+    $.getJSON($(this).attr('href'), function(player) {
+      $("h2").text(player.name);
+      $("dd").each(function(index, el) {
+        var dd = $(el);
+        var data = player[dd.data('field')];
+        dd.text(data).data('original', data);
+      });
+      $("#editPlayer").data('id', player.id);
+    });
+    $("#playerIndex").addClass('hidden');
+    $("#playerShow").removeClass('hidden');
+  });
+
+  $("dl").on('blur', 'dd', function() {
+    if ($(this).data('original') !== $(this).text()) {
+      $("#editPlayer").removeAttr('disabled');
+    }
+  });
+
+  $("#editPlayer").on('click', function() {
+    var id = $(this).data('id');
+
+    if (!id) {
+      return false;
+    }
+
+    var player = {
+      name: $("h2").text()
+    };
+    
+    $("dd").each(function(index, el) {
+      var dd = $(el);
+      var field = dd.data('field');
+      player[field] = dd.text();
+    });
+
+    $.post("/players/"+id+"/update", player, function(response) {
+      if (response.success) {
+        alert("Player updated");
+      } else {
+        alert("You suck."); 
+      }
+    }, 'json');
+  });
 });
