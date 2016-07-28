2653da56aa4521b190b850202727e035b0f88a00
Don Burks <don@donburks.com>
2016-07-28 15:50:46 -0600

    Refactored application.js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,26 +1,61 @@
 $(function() {
-  $("#loadPlayers").on('click', function() {
-    var table = $("table").find('tbody').empty();
-    $.getJSON("/players", function(players) {
+  function addPlayerRow(text, row) {
+    $("<td>").text(text).appendTo(row);
+  }
+
+  var eventHandlers = {
+    loadPlayers: function() {
+      $.getJSON("/players", ajaxHandlers.loadPlayers);
+      $("#playerIndex").removeClass('hidden');
+      $("#playerShow").addClass('hidden');
+    },
+    playerLink: function(event) {
+      event.preventDefault();
+      $.getJSON($(this).attr('href'), ajaxHandlers.playerLink);
+      $("#playerIndex").addClass('hidden');
+      $("#playerShow").removeClass('hidden');
+    },
+    dd: function() {
+      if ($(this).data('original') !== $(this).text()) {
+        $("#editPlayer").removeAttr('disabled');
+      }
+    },
+    editPlayer: function() {
+      var id = $(this).data('id');
+
+      if (!id) {
+        return false;
+      }
+
+      var player = {
+        name: $("h2").text()
+      };
+      
+      $("dd").each(function(index, el) {
+        var dd = $(el);
+        var field = dd.data('field');
+        player[field] = dd.text();
+      });
+
+      $.post("/players/"+id+"/update", player, ajaxHandlers.editPlayer, 'json');
+    }
+  };
+
+  var ajaxHandlers = {
+    loadPlayers: function(players) {
+      var table = $("table").find('tbody').empty();
       players.forEach(function(player) {
-        var row = $("<tr>");
+        var row = $("<tr>").appendTo(table);
         //Progressive Enhancement
         var a = $("<a>").attr('href', '/players/'+player.id).text(player.name).addClass('playerLink');
         $("<td>").append(a).appendTo(row);
-        $("<td>").text(player.number).appendTo(row);
-        $("<td>").text(player.position).appendTo(row);
-        $("<td>").text(player.height).appendTo(row);
-        $("<td>").text(player.weight).appendTo(row);
-        row.appendTo(table); 
+        //Fields to be displayed in table, pulled from player object
+        ['number', 'position', 'height', 'weight'].forEach(function(field) {
+          addPlayerRow(player[field], row);
+        });
       });
-    });
-    $("#playerIndex").removeClass('hidden');
-    $("#playerShow").addClass('hidden');
-  });
-
-  $("table").on('click', ".playerLink", function(event) {
-    event.preventDefault();
-    $.getJSON($(this).attr('href'), function(player) {
+    },
+    playerLink: function(player) {
       $("h2").text(player.name);
       $("dd").each(function(index, el) {
         var dd = $(el);
@@ -28,40 +63,18 @@ $(function() {
         dd.text(data).data('original', data);
       });
       $("#editPlayer").data('id', player.id);
-    });
-    $("#playerIndex").addClass('hidden');
-    $("#playerShow").removeClass('hidden');
-  });
-
-  $("dl").on('blur', 'dd', function() {
-    if ($(this).data('original') !== $(this).text()) {
-      $("#editPlayer").removeAttr('disabled');
-    }
-  });
-
-  $("#editPlayer").on('click', function() {
-    var id = $(this).data('id');
-
-    if (!id) {
-      return false;
-    }
-
-    var player = {
-      name: $("h2").text()
-    };
-    
-    $("dd").each(function(index, el) {
-      var dd = $(el);
-      var field = dd.data('field');
-      player[field] = dd.text();
-    });
-
-    $.post("/players/"+id+"/update", player, function(response) {
+    },
+    editPlayer: function(response) {
       if (response.success) {
         alert("Player updated");
       } else {
-        alert("You suck."); 
+        alert("You suck." + response.errors); 
       }
-    }, 'json');
-  });
+    }
+  };
+
+  $("#loadPlayers").on('click', eventHandlers.loadPlayers);
+  $("table").on('click', ".playerLink", eventHandlers.playerLink);
+  $("dl").on('blur', 'dd', eventHandlers.dd);
+  $("#editPlayer").on('click', eventHandlers.editPlayer);
 });
